dd8f6a9933cf5dd1cb6bca74763f8b6c1b58d408
Bharat Kotwani <bharat@imagedb.com>
2016-01-19 02:10:09 +0530

    Initial Push

--- FGMVenture/settings.py
+++ FGMVenture/settings.py
@@ -142,6 +142,7 @@ INSTALLED_APPS = (
     'django.contrib.staticfiles',
     'app',
     'compressor',
+    'gunicorn'
     
     # Uncomment the next line to enable the admin:
     # 'django.contrib.admin',
